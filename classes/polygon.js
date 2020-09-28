6d04a04abea9f960e962c840c0174873a306827f
Remo <43846137+RemGal@users.noreply.github.com>
2020-09-28 14:39:18 +0200

    Update

--- classes/polygon.js
+++ classes/polygon.js
@@ -18,15 +18,55 @@ class Polygon {
         else throw new TypeError('Wrong type of arguments in Polygon constructor.');
         if (this.points.length < 3) { throw new Error('Not enough valid arguments in Polygon constructor - it needs at least three of them.'); }
 
-        //this.points.sort(funkcja_sortujaca());
+        this.points = sortPoints(this.points);
 
         //this.rotation = W RADIANACH; <- na poczatku to bedzie 0 radianow;
 
         //this.center = SRODEK CIEZKOSCI(?) FIGURY
     }
 
-    //translate() {for(){}}
+    translate(vector) {
+        for (let point of this.points) {
+            point.translate(vector);
+        }
+    }
 
+    get perimeter() {
+        let per = 0;
+        for (i = 0; i < this.points.length-1; i++) {
+            per += distance(this.points[i], this.points[i+1]);
+        }
+        per += distance(this.points[this.points.length-1], this.points[0]);
+        return per;
+    }
+
+    get field() {
+        //Georg Alexander Pick
+        //Pick’s Theorem
+    }
 }
 
-//funkcja_sortujaca(){}
+function sortPoints(points) {
+    points = points.splice(0);
+    var p0 = {};
+    p0.y = Math.min.apply(null, points.map(p=>p.y));
+    p0.x = Math.max.apply(null, points.filter(p=>p.y == p0.y).map(p=>p.x));
+    points.sort((a,b)=>angleCompare(p0, a, b));
+    return points;
+};
+
+function angleCompare(p0, a, b) {
+    var left = isLeft(p0, a, b);
+    if (left == 0) return distCompare(p0, a, b);
+    return left;
+}
+
+function isLeft(p0, a, b) {
+    return (a.x-p0.x)*(b.y-p0.y) - (b.x-p0.x)*(a.y-p0.y);
+}
+
+function distCompare(p0, a, b) {
+    var distA = (p0.x-a.x)*(p0.x-a.x) + (p0.y-a.y)*(p0.y-a.y);
+    var distB = (p0.x-b.x)*(p0.x-b.x) + (p0.y-b.y)*(p0.y-b.y);
+    return distA - distB;
+}
